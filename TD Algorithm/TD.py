7e57fee48b642339570614f9a1d6617dcc867f01
imraviagrawal <raviagrawal@umass.edu>
2018-11-03 14:29:23 -0400

    Updated readme

--- TD Algorithm/TD.py
+++ TD Algorithm/TD.py
@@ -1,21 +1,19 @@
 # Imports
 
 import numpy as np
-from GridWorld import gridWorld
 import matplotlib.pyplot as plt
 import pickle
 import time, sys
 
 
 # TD Algorithm class
-
 class TD(object):
     # Main class to train the TD algorithm for the n number of episodes
     # the class takes the policy, alpha and lambda as the input
-    def __init__(self, lambda, alpha):
-        pass
-
-
+    def __init__(self, lambda_, alpha, env):
+        self.alpha = alpha
+        self.lambda_ = lambda_
+        self.env = env
 
     def train(self):
 
@@ -23,6 +21,3 @@ class TD(object):
         reward = 0 # todo
         td_error = None  # pass
         pass
-
-
-    def
